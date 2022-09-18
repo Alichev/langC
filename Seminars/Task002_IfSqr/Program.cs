0fd96b096a232ac487ee558b898943aaeb22c9b4
Anton Alichev <tosiakz@yandex.ru>
2022-09-18 23:02:11 +0300

    new folders

---
+++ Seminars/Task002_IfSqr/Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+
+Clear();
+Write("Введите первое число: ");
+int num1 = Convert.ToInt32(ReadLine()!);
+Write("Введите второе число: ");
+int num2 = int.Parse(ReadLine()!);
+
+int sqr = Convert.ToInt32(Math.Pow(num2, 2));
+
+if (num1 == sqr)
+{
+    WriteLine($"ДА, число {num1} является квадратом числа {num2}");
+}
+else
+{
+    WriteLine($"НЕТ, число {num1} не является квадратом числа {num2}");
+}
---
+++ Seminars/Task005_LastFigure/Program.cs
@@ -0,0 +1,6 @@
+﻿using static System.Console;
+
+Clear();
+Write("Введите трехзначное число: ");
+int num1 = Convert.ToInt32(ReadLine()!);
+Write(num1 % 10);
