5aa666beff632b66e40e7c97d24b57537f665f91
Anton Alichev <tosiakz@yandex.ru>
2022-09-18 23:23:34 +0300

    add Homework 01 task 04 All Even Numbers

---
+++ Homeworks/Homework01_task04_AllEvenNumbers/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+using static System.Console;
+
+Clear();
+Write("Введите число: ");
+int num = Convert.ToInt32(ReadLine()!);
+int num1 = 1;
+
+while (num1 <= num)
+{
+    if (num1 % 2 == 0)
+    {
+        Write($"{num1} ");
+    }
+    num1++;
+}
