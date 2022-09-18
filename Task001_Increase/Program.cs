0f5d0551bedf13b37430436672a5a0c98f99d3b2
Anton Alichev <tosiakz@yandex.ru>
2022-09-18 19:19:00 +0300

    add Task 001 Increase Sqr

---
+++ Task001_Increase/Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+
+Clear();
+Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine()!);
+Write("Введите второе число: ");
+int num2 = int.Parse(ReadLine()!);
+
+int sqr1 = num1 * num1;
+int sqr2 = Convert.ToInt32(Math.Pow(num2, 2));
+
+WriteLine($"Квадрат числа {num1} = {sqr1}");
+WriteLine($"Квадрат числа {num2} = {sqr2}");
