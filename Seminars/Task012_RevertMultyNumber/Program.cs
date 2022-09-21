3689ed9c447f57782bbdd457266aa473b6632a07
Anton Alichev <tosiakz@yandex.ru>
2022-09-21 19:51:46 +0300

    add task 012 revert multy number *

---
+++ Seminars/Task012_RevertMultyNumber/Program.cs
@@ -0,0 +1,14 @@
+﻿// развернуть многоразрядное число
+using static System.Console;
+
+Clear();
+WriteLine("Введите число: ");
+int number = int.Parse(ReadLine()!);
+string result = string.Empty;
+while (number > 0)
+{
+    result += number % 10;
+    number /= 10;
+}
+int newNumber = int.Parse(result);
+WriteLine(newNumber);
