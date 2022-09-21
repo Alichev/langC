f48f33b06b481ac30e8bd8728984fb10faaecd72
Anton Alichev <tosiakz@yandex.ru>
2022-09-21 20:59:06 +0300

    add homework for seminar 2

---
+++ Homeworks/Homework02/Task01_SecondNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа
+using static System.Console;
+
+Clear();
+int number = new Random().Next(100, 999 + 1);
+WriteLine(number);
+
+int a0 = number % 10;
+int a2 = number / 100;
+
+int a1 = (number - (a2 * 100 + a0)) / 10;
+WriteLine(a1);
---
+++ Homeworks/Homework02/Task02_ThirdNumber/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+using static System.Console;
+
+Clear();
+Write("Введите число: ");
+int number = Convert.ToInt32(ReadLine()!);
+if (number / 100 < 1)
+{
+    WriteLine("Третьей цифры справа в числе нет, в нем меньше 3 разрядов");
+    return;
+}
+
+int result = (number - (number / 1000) * 1000 - number % 100) / 100;
+WriteLine($"Третья цифра справа в числе: {result}");
