b783d32f08ed18169b06dfdd9d939707ffe36c90
Anton Alichev <tosiakz@yandex.ru>
2022-09-23 13:29:27 +0300

    add task 013 quater number with switch case

---
+++ Seminars/Task013_QuaterNumberWithSwitchCase/Program.cs
@@ -0,0 +1,57 @@
+﻿// Напишите программу, которая по заданному номеру четверти показывает диапазон возможных координат точек в этой четверти (x и y)
+using static System.Console;
+
+Clear();
+WriteLine("Введите номер четверти в системе координат: ");
+int number = int.Parse(ReadLine()!);
+
+// if (number < 1 || number > 4)
+// {
+//     WriteLine("ERROR!");
+//     return;
+// }
+// if (number == 1)
+// {
+//     WriteLine("X>0, Y>0");
+// }
+// if (number == 2)
+// {
+//     WriteLine("X<0, Y>0");
+// }
+// if (number == 3)
+// {
+//     WriteLine("X<0, Y<0");
+// }
+// if (number == 4)
+// {
+//     WriteLine("X>0, Y<0");
+// }
+
+switch (number)
+{
+    case 1:
+        {
+            WriteLine("X>0, Y>0");
+            break;
+        }
+    case 2:
+        {
+            WriteLine("X<0>, Y>0");
+            break;
+        }
+    case 3:
+        {
+            WriteLine("X<0, Y<0");
+            break;
+        }
+    case 4:
+        {
+            WriteLine("X>0, Y<0");
+            break;
+        }
+    default:
+        {
+            WriteLine("ERROR!");
+            break;
+        }
+}
