bcfe6f0f7511035ac64be7814f09bcebde1f381a
Anton Alichev <tosiakz@yandex.ru>
2022-09-23 14:19:21 +0300

    add task 014 coordinates to quater

---
+++ Seminars/Task014_CoordinatesToQuater/Program.cs
@@ -0,0 +1,40 @@
+﻿// Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 
+// и выдаёт номер четверти плоскости, в которой находится эта точка
+using static System.Console;
+
+Clear();
+Write("Введите координаты точки по оси Х: ");
+int numX = int.Parse(ReadLine()!);
+Write("Введите координаты точки по оси Y: ");
+int numY = int.Parse(ReadLine()!);
+
+if (numX == 0 && numY == 0)
+{
+    WriteLine("Это точка начала осей координат: (0,0)");
+    return;
+}
+if (numX == 0 || numY == 0)
+{
+    WriteLine("Точка находится на оси");
+    return;
+}
+if (numX > 0 && numY > 0)
+{
+    WriteLine("Точка находится в I четверти");
+    return;
+}
+if (numX > 0 && numY < 0)
+{
+    WriteLine("Точка находится в IV четверти");
+    return;
+}
+if (numX < 0 && numY > 0)
+{
+    WriteLine("Точка находится во II четверти");
+    return;
+}
+if (numX < 0 && numY < 0)
+{
+    WriteLine("Точка находится в III четверти");
+    return;
+}
