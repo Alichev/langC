6e55b15d676124fac0c4cc50ef34a27f910f593b
Anton Alichev <tosiakz@yandex.ru>
2022-09-24 19:38:12 +0300

    add task 019 paper ships

---
+++ Seminars/Task019_PaperShips/Program.cs
@@ -0,0 +1,15 @@
+﻿//Девочка и 2 мальчика делают кораблики. Девочка делает в 2 раза больше, чем оба мальчика вместе.
+// Дается общая сумма корабликов, определить, кто сколько сделал.
+using static System.Console;
+
+Clear();
+Write("Общее количество сделанных корабликов: ");
+int num = int.Parse(ReadLine()!);
+
+
+double boy = Math.Round((Convert.ToDouble(num)) / 6, 2);
+double girl = Math.Round(4 * boy, 2);
+
+
+WriteLine($"Девочка сделала {girl} корабликов");
+WriteLine($"Мальчики сделали по {boy} корабликов");
