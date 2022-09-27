b3df65c803399c0b45a0601845bd83950565834a
Anton Alichev <tosiakz@yandex.ru>
2022-09-27 21:28:30 +0300

    add task 022 multipicate product from 1 to N

---
+++ Seminars/Task022_MultiProductFrom1ToN/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая реализует метод, который принимает на вход число N и выдаёт произведение чисел от 1 до N.
+using static System.Console;
+
+Clear();
+Write("Введите число: ");
+int N = int.Parse(ReadLine()!);
+int result = GetMultiProduct(N);
+WriteLine($"Произведение чисел от 1 до N равно {result}");
+
+
+
+int GetMultiProduct(int number)
+{
+    int MultyProduct = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        MultyProduct = MultyProduct * i;
+    }
+    return MultyProduct;
+}
