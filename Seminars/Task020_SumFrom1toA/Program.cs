0b9f9a066a3158f2fe775a4ef6879e8d2c7111ec
Anton Alichev <tosiakz@yandex.ru>
2022-09-27 17:26:54 +0300

    add task 020 sum from 1 to A

---
+++ Seminars/Task020_SumFrom1toA/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая реализует метод, который принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+using static System.Console;
+
+Clear();
+Write("Введите число: ");
+int A = int.Parse(ReadLine()!);
+int result = GetSum(A);
+WriteLine($"Сумма чисел от 1 до А равна {result}");
+
+
+
+// int GetSum(int number)
+// {
+//     int sum = 0;
+//     while (number > 0)
+//     {
+//         sum = sum + number; // sum+=number;
+//         number--;
+//     }
+//     return sum;
+// }
+
+int GetSum(int number)
+{
+    int sum = 0;
+    for (int i = 1; i <= number; i++)
+    {
+        sum = sum += i;
+    }
+    return sum;
+}
