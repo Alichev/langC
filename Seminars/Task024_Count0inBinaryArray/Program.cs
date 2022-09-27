f66423f9d017d9d3b19e5fbd5e14a1ff703abf0a
Anton Alichev <tosiakz@yandex.ru>
2022-09-27 22:34:10 +0300

    add task 024 count zeroes in binary array

---
+++ Seminars/Task024_Count0inBinaryArray/Program.cs
@@ -0,0 +1,43 @@
+﻿// Напишите метод, который посчитает количество нулей в бинарном массиве
+using static System.Console;
+
+Clear();
+Write("Введите размер массива: ");
+int size = int.Parse(ReadLine()!);
+int[] finalArray = GetBinaryArray(size);
+PrintArray(finalArray);
+WriteLine($"Количество нулей в массиве: {CountZeroes(finalArray)}");
+
+
+int[] GetBinaryArray(int length)
+{
+    int[] result = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        result[i] = new Random().Next(0, 2);
+    }
+    return result;
+}
+
+void PrintArray(int[] inArray)
+{
+    Write("[");
+    for (int i = 0; i < inArray.Length - 1; i++)
+    {
+        Write($"{inArray[i]}, ");
+    }
+    WriteLine($"{inArray[inArray.Length - 1]}]");
+}
+
+int CountZeroes(int[] inArray)
+{
+    int count = 0;
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        if (inArray[i] == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
