e3cf06cc9e6def174a3ef8973ce0b1ed5fe9a2ee
Anton Alichev <tosiakz@yandex.ru>
2022-09-27 21:13:01 +0300

    add task 021 count figures in number

---
+++ Seminars/Task021_CountFiguresInNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая реализует метод, который принимает на вход число и выдаёт количество цифр в числе.
+using static System.Console;
+
+Clear();
+Write("Введите число: ");
+int number = int.Parse(ReadLine()!);
+int result = CountFigures(number);
+WriteLine($"Количестов цифр в числе {number} = {result}");
+
+
+
+int CountFigures(int N)
+{
+    int i = 0;
+    while (N > 0)
+    {
+        N = N / 10;
+        i++;
+    }
+    return i;
+}
