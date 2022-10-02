ba92db0677e5b2a92199197e799d4ad9bd7824a9
Anton Alichev <tosiakz@yandex.ru>
2022-10-02 16:09:33 +0300

    add task 027 change positive into negative and negative into positive in array

---
+++ Seminars/Task027_ChangePosToNegAndNegToPosInArray/Program.cs
@@ -0,0 +1,38 @@
+﻿// Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот
+using static System.Console;
+
+Clear();
+Write("Введите размер массива: ");
+int length = int.Parse(ReadLine()!);
+Write("Введите минимальное значение массива: ");
+int min = int.Parse(ReadLine()!);
+Write("Введите максимальное значение массива: ");
+int max = int.Parse(ReadLine()!);
+
+int[] Array = GetArray(length, min, max);
+WriteLine($"[{String.Join(", ", Array)}]");
+ChangeArray(Array);
+WriteLine($"[{String.Join(", ", Array)}]");
+
+
+
+
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    Random rnd = new Random();
+    int[] result = new int[size];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = rnd.Next(minValue, maxValue + 1);
+    }
+    return result;
+}
+
+void ChangeArray(int[] inArray)
+{
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        inArray[i] = -inArray[i];
+    }
+}
