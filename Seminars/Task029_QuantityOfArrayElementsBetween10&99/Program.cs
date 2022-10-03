59498545f5d089632a436f8243336225ebc2b97d
Anton Alichev <tosiakz@yandex.ru>
2022-10-03 13:03:12 +0300

    add task 029 and 030

---
+++ Seminars/Task029_QuantityOfArrayElementsBetween10&99/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задайте одномерный массив из 123 случайных чисел. 
+// Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
+using static System.Console;
+
+Clear();
+int[] Array = GetArray(20);
+WriteLine($"[{String.Join(", ", Array)}]");
+FindQuantityInArray(Array);
+
+
+int[] GetArray(int size)
+{
+    Random rnd = new Random();
+    int[] result = new int[size];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = rnd.Next(-500, 500);
+    }
+    return result;
+}
+
+void FindQuantityInArray(int[] inArray)
+{
+    int count = 0;
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        if (inArray[i] >= 10 && inArray[i] <= 99)
+        {
+            count++;
+        }
+    }
+    WriteLine($"В интервал от 10 до 99 попадает {count} элементов массива");
+}
---
+++ Seminars/Task030_MultyplyArrayNumbers/Program.cs
@@ -0,0 +1,45 @@
+﻿// Найдите произведение пар чисел в одномерном массиве. 
+// Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
+using static System.Console;
+
+Clear();
+int[] Array = GetArray(11);
+WriteLine($"[{String.Join(", ", Array)}]");
+
+
+int[] resultArray = GetMultiplyNumbers(Array);
+WriteLine($"[{String.Join(", ", resultArray)}]");
+
+
+
+
+
+
+
+
+int[] GetArray(int size)
+{
+    Random rnd = new Random();
+    int[] result = new int[size];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = rnd.Next(-5, 6);
+    }
+    return result;
+}
+
+int[] GetMultiplyNumbers(int[] inArray)
+{
+    int newArrLength = inArray.Length % 2 == 0 ? inArray.Length / 2 : inArray.Length / 2 + 1;
+    int[] newArr = new int[newArrLength];
+
+    for (int i = 0; i < inArray.Length / 2; i++)
+    {
+        newArr[i] = inArray[i] * inArray[inArray.Length - 1 - i];
+    }
+    if (inArray.Length % 2 != 0)
+    {
+        newArr[newArrLength - 1] = inArray[inArray.Length / 2];
+    }
+    return newArr;
+}
