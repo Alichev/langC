1cf98bddc2fae1ed7a5fd57edbf050c5a800a818
Anton Alichev <tosiakz@yandex.ru>
2022-10-04 09:04:44 +0300

    correction in example 016

--- Lections/Example016_Fibonacci/Program.cs
+++ Lections/Example016_Fibonacci/Program.cs
@@ -4,7 +4,7 @@
 int Fibonacci(int n)
 {
     if (n == 1 || n == 2) return 1;
-    else Fibonacci(n - 1) + Fibonacci(n - 2);
+    else return Fibonacci(n - 1) + Fibonacci(n - 2);
 }
 for (int i = 1; i < 10; i++)
 {
