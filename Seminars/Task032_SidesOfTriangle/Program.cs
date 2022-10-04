faeb001fe5c360956c711221ced55c2cbf0c7226
Anton Alichev <tosiakz@yandex.ru>
2022-10-04 15:44:48 +0300

    add task 032 sides of triangle

---
+++ Seminars/Task032_SidesOfTriangle/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход три числа и проверяет, 
+// может ли существовать треугольник с сторонами такой длины.
+using static System.Console;
+
+Clear();
+WriteLine("Введите длины сторон треугольника через пробел: ");
+string[] parameters = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+int a = int.Parse(parameters[0]);
+int b = int.Parse(parameters[1]);
+int c = int.Parse(parameters[2]);
+CheckTriangle(a, b, c);
+
+
+void CheckTriangle(int x, int y, int z)
+{
+    if (x + y > z && x + z > y && y + z > x)
+    {
+        WriteLine($"Треугольник со сторонами {x}, {y} и {z} может существовать");
+    }
+    else
+    {
+        WriteLine($"Треугольник со сторонами {x},{y} и {z} НЕ МОЖЕТ существовать");
+    }
+}
