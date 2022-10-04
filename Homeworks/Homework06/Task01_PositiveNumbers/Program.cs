5fae2c05d550e78dd529e972382f7bffc382e6e8
Anton Alichev <tosiakz@yandex.ru>
2022-10-04 19:50:20 +0300

    add homework 06

---
+++ Homeworks/Homework06/Task01_PositiveNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь
+using static System.Console;
+
+Clear();
+WriteLine("Введите 5 чисел через пробел: ");
+string[] pars = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+int[] array = { int.Parse(pars[0]), int.Parse(pars[1]), int.Parse(pars[2]), int.Parse(pars[3]), int.Parse(pars[4]) };
+WriteLine($"Вы ввели {CountPositiveNumbers(array)} чисел больше нуля");
+
+
+
+
+
+int CountPositiveNumbers(int[] inArray)
+{
+    int count = 0;
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        if (inArray[i] > 0)
+        {
+            count += 1;
+        }
+    }
+    return count;
+}
---
+++ Homeworks/Homework06/Task02_CrossLinesPoint/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем
+
+using static System.Console;
+Clear();
+WriteLine("Введите значение b1: ");
+int b1 = int.Parse(ReadLine()!);
+WriteLine("Введите значение k1: ");
+int k1 = int.Parse(ReadLine()!);
+WriteLine("Введите значение b2: ");
+int b2 = int.Parse(ReadLine()!);
+WriteLine("Введите значение k2: ");
+int k2 = int.Parse(ReadLine()!);
+WriteLine($"Координаты точки пересечения: ({String.Join(", ", GetCrossPoint(b1, k1, b2, k2))})");
+
+
+double[] GetCrossPoint(double bb1, double kk1, double bb2, double kk2)
+{
+    double[] result = new double[2];
+    result[0] = (bb2 - bb1) / (kk1 - kk2);
+    result[1] = kk1 * (bb2 - bb1) / (kk1 - kk2) + bb1;
+    return result;
+}
